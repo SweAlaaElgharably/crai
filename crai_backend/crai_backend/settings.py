64b74729ad935712c2db47baccbafee95e2dbc91
SweAlaaElgharably <swe.alaaelgharably@gmail.com>
2026-07-19 18:56:08 +0300

    update

--- crai_backend/crai_backend/settings.py
+++ crai_backend/crai_backend/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "user.User"
 
