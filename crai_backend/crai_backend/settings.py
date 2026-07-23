727c2ddfd862ca841db790f32fa56cc44b187c0d
SweAlaaElgharably <swe.alaaelgharably@gmail.com>
2026-07-23 12:25:45 +0300

    update settings

--- crai_backend/crai_backend/settings.py
+++ crai_backend/crai_backend/settings.py
@@ -99,23 +99,23 @@ WSGI_APPLICATION = 'crai_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ.get("POSTGRES_DB"),
-#         "USER": os.environ.get("POSTGRES_USER"),
-#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#         "HOST": os.environ.get("POSTGRES_HOST", "db"),
-#         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -229,26 +229,26 @@ DJOSER = {
     },
 }
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.hostinger.com'
-# EMAIL_PORT = 465
-# EMAIL_USE_SSL = True
-# EMAIL_HOST_USER = 'admin@cr-ai.cloud' 
-# EMAIL_HOST_PASSWORD = 'peig-ilhm-z6yh-3gai' 
-# DEFAULT_FROM_EMAIL = 'CRAI <admin@cr-ai.cloud>'
-# EMAIL_USE_TLS = False
-# SERVER_EMAIL = "admin@cr-ai.cloud"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.hostinger.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'admin@cr-ai.cloud' 
+EMAIL_HOST_PASSWORD = 'peig-ilhm-z6yh-3gai' 
+DEFAULT_FROM_EMAIL = 'CRAI <admin@cr-ai.cloud>'
+EMAIL_USE_TLS = False
+SERVER_EMAIL = "admin@cr-ai.cloud"
 
-EMAIL_BACKEND = 'user.email_backend.UnverifiedEmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 465
-EMAIL_PORT = 587
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'swe.alaaelgharably@gmail.com' 
-EMAIL_HOST_PASSWORD = 'mhed obin yrfk pzjw' 
-DEFAULT_FROM_EMAIL = 'CRAI <swe.alaaelgharably@gmail.com>'
-EMAIL_USE_TLS = True
-SERVER_EMAIL = "swe.alaaelgharably@gmail.com"
+# EMAIL_BACKEND = 'user.email_backend.UnverifiedEmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# # EMAIL_PORT = 465
+# EMAIL_PORT = 587
+# EMAIL_USE_SSL = False
+# EMAIL_HOST_USER = 'swe.alaaelgharably@gmail.com' 
+# EMAIL_HOST_PASSWORD = 'mhed obin yrfk pzjw' 
+# DEFAULT_FROM_EMAIL = 'CRAI <swe.alaaelgharably@gmail.com>'
+# EMAIL_USE_TLS = True
+# SERVER_EMAIL = "swe.alaaelgharably@gmail.com"
 
 
 CORS_ALLOWED_ORIGINS = [
